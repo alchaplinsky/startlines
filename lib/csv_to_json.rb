dc47bdc406693f880f6ab3fe793c6dbdf0173f4d
Alex Chaplinsky <alchaplinsky@gmail.com>
2015-09-16 13:53:38 +0700

    Added Viber timeline

--- lib/csv_to_json.rb
+++ lib/csv_to_json.rb
@@ -1,7 +1,7 @@
 require 'multi_json'
 require 'csv'
 
-body = File.read('/Users/ac/Startup/startlines/json/inprogress/ebay.csv')
+body = File.read('/Users/ac/Startup/startlines/json/inprogress/viber.csv')
 csv = CSV.new(body, :headers => true, :header_converters => :symbol)
 json = csv.to_a.map {|row| row.to_hash }
-File.write '/Users/ac/Startup/startlines/json/inprogress/ebay_entries.json', MultiJson.dump(json, pretty: true)
+File.write '/Users/ac/Startup/startlines/json/inprogress/viber_entries.json', MultiJson.dump(json, pretty: true)
