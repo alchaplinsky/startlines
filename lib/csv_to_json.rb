21a9a4bb0f18ca252a309969ffc28a0927b25809
Alex Chaplinsky <alchaplinsky@gmail.com>
2015-09-24 19:36:47 +0700

    Added amazon timeline

--- lib/csv_to_json.rb
+++ lib/csv_to_json.rb
@@ -1,7 +1,7 @@
 require 'multi_json'
 require 'csv'
 
-body = File.read('/Users/ac/Startup/startlines/json/inprogress/viber.csv')
+body = File.read('/Users/ac/Startup/startlines/json/inprogress/amazon.csv')
 csv = CSV.new(body, :headers => true, :header_converters => :symbol)
 json = csv.to_a.map {|row| row.to_hash }
-File.write '/Users/ac/Startup/startlines/json/inprogress/viber_entries.json', MultiJson.dump(json, pretty: true)
+File.write '/Users/ac/Startup/startlines/json/inprogress/amazon.json', MultiJson.dump(json, pretty: true)
