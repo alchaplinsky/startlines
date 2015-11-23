e6522351e80d198a88196e0d22aa0ea8aa9f0473
Alex Chaplinsky <alchaplinsky@gmail.com>
2015-11-23 14:14:50 +0700

    Added medium

--- lib/csv_to_json.rb
+++ lib/csv_to_json.rb
@@ -1,7 +1,7 @@
-require 'multi_json'
-require 'csv'
-
-body = File.read('/Users/ac/Startup/startlines/json/inprogress/github.csv')
-csv = CSV.new(body, :headers => true, :header_converters => :symbol)
-json = csv.to_a.map {|row| row.to_hash }
-File.write '/Users/ac/Startup/startlines/json/inprogress/github.json', MultiJson.dump(json, pretty: true)
+# require 'multi_json'
+# require 'csv'
+#
+# body = File.read('/Users/ac/Startup/startlines/json/inprogress/medium.csv')
+# csv = CSV.new(body, :headers => true, :header_converters => :symbol)
+# json = csv.to_a.map {|row| row.to_hash }
+# File.write '/Users/ac/Startup/startlines/json/inprogress/medium.json', MultiJson.dump(json, pretty: true)
